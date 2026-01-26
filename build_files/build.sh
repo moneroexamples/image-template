773679aa4b597fb7363eda95229792e1ce7bddef
Your Name <you@example.com>
2026-01-27 07:09:13 +0800

    virtualbox 5 rpmfusion2

--- build_files/build.sh
+++ build_files/build.sh
@@ -31,6 +31,8 @@ dnf5  install -y mc tigervnc tigervnc-server \
 #dnf5 install -y VirtualBox-7.2
 #sudo usermod -a -G vboxusers mwo
 
+dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
 dnf5 install -y VirtualBox
 
 #dnf5 install -y  plasma-workspace-x11
